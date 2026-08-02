304f9364e15d1c7d6a3542c1f7a49633ce3086c6
youcandonothingwiththisacc-pixel <youcandonothingwiththisacc@gmail.com>
2026-08-02 21:55:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let selectedReward = "";
+
+function selectReward(type) {
+    selectedReward = type;
+
+    document.querySelectorAll(".option").forEach(btn => {
+        btn.style.background = "#1d1d2f";
+    });
+
+    event.target.style.background = "#6b00ff";
+}
+
 function continuePage() {
     const user = document.getElementById("username").value;
 
@@ -6,5 +18,10 @@ function continuePage() {
         return;
     }
 
-    window.location.href = "https://locked-content.com/?9ded58f";
+    if (selectedReward === "") {
+        alert("Please select an option.");
+        return;
+    }
+
+   window.location.href = "https://locked-content.com/?9ded58f";
 }
