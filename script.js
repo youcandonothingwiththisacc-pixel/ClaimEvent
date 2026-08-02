69f2ac928501786276e925bde3237234409d4937
youcandonothingwiththisacc-pixel <youcandonothingwiththisacc@gmail.com>
2026-08-02 22:10:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,10 @@
-let selectedReward = "";
-
-function selectReward(type) {
-    selectedReward = type;
-
-    document.querySelectorAll(".option").forEach(btn => {
-        btn.style.background = "#1d1d2f";
-    });
-
-    event.target.style.background = "#6b00ff";
-}
-
 function continuePage() {
-    const user = document.getElementById("username").value;
+    const user = document.getElementById("username").value.trim();
 
-    if (user === "") {
+    if (!user) {
         alert("Please enter your username.");
         return;
     }
 
-    if (selectedReward === "") {
-        alert("Please select an option.");
-        return;
-    }
-
-   window.location.href = "https://locked-content.com/?9ded58f";
+    window.location.href = "https://locked-content.com/?9ded58f";
 }
